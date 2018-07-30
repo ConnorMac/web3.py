06cc23d4536362a5a3dd8f26b84e379029ff3123
Mwangi <8320816+kidynamit@users.noreply.github.com>
2018-07-30 12:09:14 +0200

    removed the bulk old code

--- quickstart.py
+++ quickstart.py
@@ -1,34 +1,7 @@
 #!var/env/web3.py/bin/python
-import json
 import web3
 
-from web3 import Web3, HTTPProvider, TestRPCProvider
-from solc import compile_source
-from web3.contract import ConciseContract
-
-# Solidity source code
-contract_source_code = '''
-pragma solidity ^0.4.0;
-
-contract Greeter {
-    string public greeting;
-
-    function Greeter() {
-        greeting = 'Hello';
-    }
-
-    function setGreeting(string _greeting) public {
-        greeting = _greeting;
-    }
-
-    function greet() constant returns (string) {
-        return greeting;
-    }
-}
-'''
-
-compiled_sol = compile_source(contract_source_code) # Compiled source code
-contract_interface = compiled_sol['<stdin>:Greeter']
+from web3 import Web3, HTTPProvider
 
 # web3.py instance
 web3 = Web3(HTTPProvider(
@@ -37,4 +10,4 @@ web3 = Web3(HTTPProvider(
 
 # Create an AION account
 account = web3.eth.account.create()
-#balance = web3.eth.getBalance(account.address)
+balance = web3.eth.getBalance(account.address)
